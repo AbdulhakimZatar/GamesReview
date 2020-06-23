1be5399f1a49579d6f7bc57eb99e59e7bf4f07a5
AbdulhakimZatar <hakimzatar@gmail.com>
2020-06-23 17:53:59 +0300

    Added js

---
+++ js/age.js
@@ -0,0 +1,6 @@
+var age = confirm("Are you 18 or over years old?")
+if (age == true){
+
+} else {
+    window.location.replace('index.html');
+}
---
+++ js/lab06.js
@@ -0,0 +1,22 @@
+'use strict';
+console.log("test");
+
+
+var reviewer = prompt('Enter your name:');
+var gamename = prompt('Enter name of the game:');
+var rating = prompt('Enter your rating for the game from 0-10:');
+var rating_save;
+
+if (rating >=10) {
+    rating_save = "Best game i have ever tried.";
+} else if (rating >=8){
+    rating_save = "Excellent game to spend your time on it.";
+} else if (rating >= 5) {
+    rating_save = "Normal game, if you don't have anything to play.";
+} else if (rating >= 2){
+    rating_save = "Bad game, not recommanded at all.";
+} else if (rating >= 0) {
+    rating_save = "Wrost game ever.";
+} else {
+    rating_save = "Wrong value";
+}
