afcf53a72ba985a9aa8823f4b501f85a05cf49f4
AbdulhakimZatar <hakimzatar@gmail.com>
2020-06-23 18:02:56 +0300

    fix age.js

--- js/age.js
+++ js/age.js
@@ -1,7 +1,8 @@
+prompt("test");
 var age = confirm("Are you 18 or over years old?")
 if (age == true){
 
 } else {
-    prompt("Sorry you are under age, you are not allowed to submit review if you under 18");
+    prompt ("Sorry you are under age, you are not allowed to submit review if you under 18");
     window.location.replace('index.html');
 }
