b196dad262b44f33cf6feb900900c3ea49e78d9b
AbdulhakimZatar <hakimzatar@gmail.com>
2020-06-24 12:21:58 +0300

    function js

--- js/age.js
+++
@@ -1,7 +0,0 @@
-var age = confirm("Are you 18 or over years old?")
-if (age == true){
-
-} else {
-    alert ("Sorry you are under age, you are not allowed to submit review if you under 18");
-    window.location.replace('index.html');
-}
--- js/lab06.js
+++ js/lab06.js
@@ -1,7 +1,6 @@
 'use strict';
-console.log("test");
 
-
+function review(){
 var reviewer = prompt('Enter your name:');
 var gamename = prompt('Enter name of the game:');
 var rating = prompt('Enter your rating for the game from 0-10:');
@@ -19,7 +18,26 @@ if (rating >=10) {
     rating_save = "Wrost game ever.";
 } else {
     rating_save = "Wrong value";
+    document.getElementById("gametitle").innerHTML = gamename;
+    document.getElementById("reviewer").innerHTML += rating_save;
+    document.getElementById("rating").innerHTML += reviewer;
 }
+}
+
+
+function age() {
+    var age = confirm("Are you 18 or over years old?")
+if (age == true){
+    review();
+} else {
+    alert ("Sorry you are under age, you are not allowed to submit review if you under 18");
+    window.location.replace('index.html');
+    return;
+}
+}
+
+age();
+
 
 function color(){
     var textcolor = prompt('What color text would you like the review to be in:');
